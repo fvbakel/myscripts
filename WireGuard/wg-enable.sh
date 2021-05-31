9e951b2a107dba6f29b56ee14ac2258f5b6d54a8
Frank van Bakel <fvbakel@gmail.com>
2021-05-31 22:33:07 +0200

    Changed to popup info notification

--- WireGuard/wg-enable.sh
+++ WireGuard/wg-enable.sh
@@ -11,15 +11,37 @@
 # ```file='/etc/wireguard/wg0.conf'```
 # Now import it using the nmcli command: 
 # ```sudo nmcli connection import type wireguard file "$file"```
-# Rename profile wg0 as home-vpn: 
-# ```nmcli connection modify wg0 connection.id "home-vpn"```
+# Rename profile wg0 as $con_name: 
+# ```nmcli connection modify wg0 connection.id "$con_name"```
 #
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+RED="\e[31m"
+GREEN="\e[32m"
+NC="\e[0m"
 
-printf "Wireguard connection home-vpn set to: ${GREEN}$1${NC}\n"
+con_name=$1
+action=$2
+
+
 #sudo wg-quick $1 wg0
-nmcli connection $1 home-vpn
+OUTPUT=$(nmcli connection $action $con_name 2>&1)
+
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    printf "Wireguard connection $con_name is ${GREEN}$action${NC}\n"
+    zenity  --info --ellipsize --timeout=2 \
+            --text="Wireguard connection $con_name is\n$action" \
+            --title="WireGuard" \
+            --window-icon=$SCRIPT_DIR/icons/WireGuard.png \
+            --icon-name=go-$action
+else
+    printf "${RED}Error${NC} setting Wireguard connection $con_name to ${GREEN}$action${NC}\n"
+    zenity  --error --ellipsize \
+            --text="Error setting Wireguard connection $con_name to \n$action\n\nDetails:\n$OUTPUT" \
+            --title="WireGuard" \
+            --window-icon=$SCRIPT_DIR/icons/WireGuard.png \
+            --icon-name=error
+fi
 
 printf "\n"
 
